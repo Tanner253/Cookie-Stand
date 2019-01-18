9730910afa88ba184039adfc9f41669c312bf795
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-01-17 21:18:46 -0800

    added css styling to my markup

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var salesForm = document.getElementById('sales-form');
 
 
 var time = ['6 am','7 am','8 am', '9 am', '10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm','8 pm', ];
-var Table = document.getElementById('cookie-Table');
+var Table = document.getElementById('cookieTable');
 
 Store.prototype.calcTotals = function(){ //get random intiger and store it in prototype:"numbers"
   for(var i = 0 ; i < time.length; i++){ //loop iteration for each our
